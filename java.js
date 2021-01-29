da5a622296d4b43bb9589ca0058f280fbfc088ca
joemc9011 <joemc9011@gmail.com>
2021-01-29 12:09:13 -0500

    more old code

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 const button = document.querySelector(".start");
 const quizpage = document.querySelector(".quizbody");
 const questionNum = document.querySelector(".questionheader");
-const questions = document.querySelector(".questions");
-const answer = document.querySelector("#answer");
+const choicesEL = document.querySelector(".choicess");
+const answerEl = document.querySelector("#answer");
 const scores = document.querySelector("#highscores");
 const timer = document.querySelector("#timer");
 
@@ -10,22 +10,22 @@ const timer = document.querySelector("#timer");
 var questions = [
     {
         question: "What does HTML stand for?",
-        choice: ["Hover Typo Makeup Letters", "Heavy Text Markup Leverage", "Hyper Text Markup Language"],
+        choices: ["Hover Typo Makeup Letters", "Heavy Text Markup Leverage", "Hyper Text Markup Language"],
         answer: "Hyper Text Markup Language"
     },
     {
         question: "Which does not belong",
-        choice: ["Java", "Fire", "Python"],
+        choices: ["Java", "Fire", "Python"],
         answer: "Fire"
     },
     {
         question: "The action of doing something over and over again, repeating code",
-        choice: ["Repeat", "Program", "Loop"],
+        choices: ["Repeat", "Program", "Loop"],
         answer: "Loop"
     },
     {
         question: "An error, or mistake, in a program that prevents the program from being run correctly",
-        choice: ["Bug", "Glitch", "Lemon"],
+        choices: ["Bug", "Glitch", "Lemon"],
         answer: "Bug"
     },
 ];
@@ -37,10 +37,66 @@ var correctCount = 0;
 var time = 15;
 var intervalId;
 
-function startQuiz (){
-    time = 15
+function startQuiz() {
+    time = 15;
     timer.innerHTML = time;
+    button.style.visibility = "hidden";
     
 
+    function updateTime() {
+        time--;
+        if (time <= 0) {
+            endQuiz();
+        }
+    };
 
-  }
+    intervalId = setInterval(updateTime, 1000);
+
+    };
+
+    function renderQuestions (){
+
+        questionEl.textContent = questions[questionIndex].question;
+
+        choicesEl.innerHTML = "";
+        answerEl.innerHTML = "";
+    
+        var choicess = questions[questionIndex].choicess;
+        var choicessLenth = choicess.length;
+    
+        for (var i = 0; i < choicessLenth; i++) {
+            var questionListItem = document.createElement("li");
+            questionListItem.textContent = choicess[i];
+            optionListEl.append(questionListItem);
+    }
+}
+
+function nextQuestion () {
+    questionIndex++;
+    if (questionIndex === questions.length) {
+        time = 0;
+      }
+      else {
+        renderQuestion();
+
+      }
+    }
+
+
+    function checkAnswer(event) {
+        clearInterval(intervalId);
+        if (event.target.matches("li")) {
+          var answer = event.target.textContent;
+          if (answer === questions[questionIndex].answer) {
+            answerEl.textContent = "Correct";
+            correctCount++;
+      
+            
+          } else {
+            answerEl.textContent = "Incorrect";
+            time = time - 2;
+            timerEl.textContent = time;
+          }
+        }
+        setTimeout(nextQuestion, 500);
+      }
